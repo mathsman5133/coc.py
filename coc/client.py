b2e01376e57710cc0f12ed9131d8b2036430001c
Jabbey92 <32350542+Jabbey92@users.noreply.github.com>
2019-04-17 20:09:11 -0400

    Update client.py
    Yet another fix to the tokens checks

--- coc/client.py
+++ coc/client.py
@@ -117,14 +117,17 @@ class Client:
 
     def __init__(self, tokens=None, loop=None, email=None, password=None, update_tokens=False):
         self.loop = self.loop = loop or asyncio.get_event_loop()
+        has_auth = not(password or email)
         
         if tokens:
             if isinstance(tokens, str): tokens = [tokens]
             elif isinstance(tokens, list): pass
             else: raise RuntimeError('tokens must be either a str or list of str tokens')
-        #no need for else as None is handled in HTTPClient
+        else:
+            if not has_auth:
+                raise RuntimeError('An email and password must be set if no tokens are provided')
 
-        if update_tokens and not(password or email):
+        if update_tokens and :
                 raise RuntimeError('An email and password must be set if update_tokens is True')
 
         self.http = HTTPClient(client=self, tokens=tokens, loop=self.loop, email=email,
