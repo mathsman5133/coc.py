2695f86cb0afce9c021be2d88ae4fcc6a63a2a95
mathsman5133 <mathsman5132@gmail.com>
2019-04-22 00:16:13 +1000

    Bump versions to v0.1.0 and fix formatting for changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-version = '0.0.6'
+version = '0.1.0'
 
 readme = ''
 with open('README.rst') as f:
