0a2b7dd6d7bb908d5dcf1d8955d0fe133b6bbc60
mathsman5133 <mathsman5132@gmail.com>
2019-04-23 18:15:55 +1000

    Add enum `CacheType`, returning the string of cache type used. - Passing in this over a string is preferred as the string names may change, but these won't.

---
+++ coc/enums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class CacheType(Enum):
+    search_clans = 'cache_search_clans'
+    war_clans = 'cache_war_clans'
+
+    cache_search_players = 'cache_search_players'
+    war_players = 'cache_war_players'
+
+    current_wars = 'cache_current_wars'
+    war_logs = 'cache_war_logs'
+
+    league_groups = 'cache_league_groups'
+    league_wars = 'cache_league_wars'
+
+    locations = 'cache_locations'
+    leagues = 'cache_leagues'
+    seasons = 'cache_seasons'
+
+    def __str__(self):
+        return self.value
