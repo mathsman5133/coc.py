3920d6e25ce96ba173b74d327aaaf45ebfd25dc0
mathsman5133 <mathsman5132@gmail.com>
2019-05-19 20:35:03 +1000

    Bump version to v0.2.0a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-version = '0.1.3'
+version = '0.2.0a'
 
 readme = ''
 with open('README.rst') as f:
