6cda7f9fb76b71e322b7cf2f54384c5c8064fd31
mathsman5133 <mathsman5132@gmail.com>
2019-06-18 19:59:49 +1000

    Update to v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = []
 with open(os.path.join(os.getcwd(), 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
-version = '0.2.0'
+version = '0.2.1'
 
 readme = ''
 with open('README.rst') as f:
