c6fd3761f4a78b22532854c616facffd07bb90a9
mathsman5133 <mathsman5132@gmail.com>
2019-11-02 16:21:09 +1100

    BugFix `on_war_state_change` to properly fire when `preparation` and `warEnded` are done.

--- coc/events.py
+++ coc/events.py
@@ -513,7 +513,11 @@ class EventsClient(Client):
         if war.state == cached_war.state:
             return
 
-        if war.state not in ['preparation', 'inWar', 'warEnded']:
+        if war.state == 'preparation':
+            self.dispatch('on_war_state_change', 'preparation', war)
+            return
+
+        if war.state not in ['inWar', 'warEnded']:
             return
 
         in_war_task, war_ended_task = self._check_state_task_status(war.clan_tag)
@@ -523,7 +527,7 @@ class EventsClient(Client):
             self._add_state_task(war.clan_tag, 'inWar', task)
 
         if not war_ended_task or (war.end_time.time != cached_war.end_time.time):
-            task = self.loop.create_task(self._wait_for_state_change('inWar', war))
+            task = self.loop.create_task(self._wait_for_state_change('warEnded', war))
             self._add_state_task(war.clan_tag, 'warEnded', task)
 
     async def _update_wars(self):
