0e2970e9f0fa8dd0df9cea726c44f9730ffa944f
mathsman5133 <mathsman5132@gmail.com>
2020-06-27 17:48:02 +1000

    version bump and bump sphinx to latest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ REQUIREMENTS = []
 with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
     REQUIREMENTS = f.read().splitlines()
 
-VERSION = "0.3.3"
+VERSION = "1.0.0a"
 
 README = ""
 with open("README.rst") as f:
