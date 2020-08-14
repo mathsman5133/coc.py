d3ad0c2c556ef86e967639c1da778678e584ca33
mathsman5133 <38833292+mathsman5133@users.noreply.github.com>
2020-08-14 11:57:50 +1000

    Simplify example setup.

--- examples/events.py
+++ examples/events.py
@@ -7,22 +7,20 @@ client = coc.login("email", "pass", client=coc.EventsClient)
 async def on_clan_member_join(member, clan):
     print("{} just joined our clan {}!".format(member.name, clan.name))
 
-
+@client.event
 async def on_clan_member_leave(member, clan):
     print("Oh no! {} just left our clan {}!".format(member.name, clan.name))
 
-
-async def when_someone_attacks(attack, war):
+@client.event
+async def on_war_attack(attack, war):
     print(
         "{} just attacked {} for {} stars! It was a {} war.".format(
             attack.attacker.name, attack.defedender.name, attack.stars, war.type
         )
     )
 
-
-client.add_events(on_clan_member_join, function_dicts={"on_war_attack": when_someone_attacks})
-client.add_clan_update("tag", "another tag", retry_interval=30)
-client.add_war_update("clan tag", retry_interval=60)
-client.start_updates("all")
+tags = ["#tag", "#tag2", "#tag3"]
+client.add_clan_update(tags, retry_interval=30)
+client.add_war_update(tags, retry_interval=300)
 
 client.run_forever()
