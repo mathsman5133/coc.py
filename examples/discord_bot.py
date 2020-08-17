4cc53836bd8e49bd8c86a4a493b2b08c969d2d53
Anubhav1603 <anubhavsinha110@gmail.com>
2020-08-17 23:53:26 +0530

    changed made according to mathsman suggestion for pr #54

--- examples/discord_bot.py
+++ examples/discord_bot.py
@@ -39,6 +39,10 @@ async def on_event_error(event_name, exception, *args, **kwargs):
 
 @bot.command()
 async def player_heroes(ctx, player_tag):
+    if not utils.is_valid_tag(player_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
     player = await coc_client.get_player(player_tag)
 
     to_send = ""
@@ -50,51 +54,60 @@ async def player_heroes(ctx, player_tag):
 
 @bot.command()
 async def clan_info(ctx, clan_tag):
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
     clan = await coc_client.get_clan(clan_tag)
 
-    for a in clan.members:
-        if a.role == a.role.leader:
-            leader = a
     if clan.public_war_log is False:
-        log = 'Private'
+        log = "Private"
     else:
-        log = 'Public'
+        log = "Public"
     e = discord.Embed(colour=discord.Colour.green())
     e.set_thumbnail(url=clan.badge.url)
-    e.add_field(name='Clan Name', value=f'{clan.name}({clan.tag})\n[Open in game]({clan.share_link})', inline=False)
-    e.add_field(name='Clan Level', value=clan.level, inline=False)
+    e.add_field(name="Clan Name", value=f"{clan.name}({clan.tag})\n[Open in game]({clan.share_link})", inline=False)
+    e.add_field(name="Clan Level", value=clan.level, inline=False)
     e.add_field(name="Description", value=clan.description, inline=False)
-    e.add_field(name="Leader", value=f'{leader}', inline=False)
-    e.add_field(name='Clan Type', value=clan.type, inline=False)
-    e.add_field(name='Location', value=clan.location, inline=False)
-    e.add_field(name='Total Clan Trophies', value=clan.points, inline=False)
-    e.add_field(name='Total Clan Versus Trophies', value=clan.versus_points, inline=False)
-    e.add_field(name='WarLog Type', value=log, inline=False)
-    e.add_field(name= 'Required Trophies', value=clan.required_trophies, inline=False)
-    e.add_field(name='War Win Streak', value = clan.war_win_streak, inline=False)
-    e.add_field(name='War Frequency', value= clan.war_frequency, inline=False)
-    e.add_field(name='Clan War League Rank', value= clan.war_league, inline=False)
-    e.add_field(name='Clan Labels', value ='\n'.join(label.name for label in clan.labels), inline=False)
-    e.add_field(name='Member Count', value = f'{clan.member_count}/50', inline=False)
+    e.add_field(name="Leader", value=clan.get_member_by(role=coc.Role.leader), inline=False)
+    e.add_field(name="Clan Type", value=clan.type, inline=False)
+    e.add_field(name="Location", value=clan.location, inline=False)
+    e.add_field(name="Total Clan Trophies", value=clan.points, inline=False)
+    e.add_field(name="Total Clan Versus Trophies", value=clan.versus_points, inline=False)
+    e.add_field(name="WarLog Type", value=log, inline=False)
+    e.add_field(name="Required Trophies", value=clan.required_trophies, inline=False)
+    e.add_field(name="War Win Streak", value=clan.war_win_streak, inline=False)
+    e.add_field(name="War Frequency", value=clan.war_frequency, inline=False)
+    e.add_field(name="Clan War League Rank", value=clan.war_league, inline=False)
+    e.add_field(name="Clan Labels", value="\n".join(label.name for label in clan.labels), inline=False)
+    e.add_field(name="Member Count", value=f"{clan.member_count}/50", inline=False)
     e.add_field(name="Clan Record", value=f"Won - {clan.war_wins}\nLost - {clan.war_losses}\n Draw - {clan.war_ties}", inline=False)
     await ctx.send(embed=e)
 
 
 @bot.command()
 async def clan_member(ctx, clan_tag):
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
     clan = await coc_client.get_clan(clan_tag)
-    member = ''
+
+    member = ""
     for i, a in enumerate(clan.members, start=1):
-        member += f'``{i}.`` {a.name}\n'
-    embed = discord.Embed(colour=discord.Colour.red(), title= f'Members of {clan.name}',description= f'{member}')
+        member += f'`{i}.` {a.name}\n'
+    embed = discord.Embed(colour=discord.Colour.red(), title=f'Members of {clan.name}', description=member)
     embed.set_thumbnail(url=clan.badge.url)
     embed.set_footer(text=f'Total Members - {clan.member_count)}/50')
     await ctx.send(embed=embed)
 
 
-
 @bot.command()
 async def current_war_status(ctx, clan_tag):
+    if not utils.is_valid_tag(clan_tag):
+        await ctx.send("You didn't give me a proper tag!")
+        return
+
     e = discord.Embed(colour=discord.Colour.blue())
 
     try:
