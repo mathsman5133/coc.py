335a8bcb4f4738c3bf49dd6b1c9f6e36053c0da9
mathsman <mathsman5132@gmail.com>
2021-04-13 12:24:07 +1000

    Version Bump to v1.2.0a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ REQUIREMENTS = []
 with open(os.path.join(os.getcwd(), "requirements.txt")) as f:
     REQUIREMENTS = f.read().splitlines()
 
-VERSION = "1.1.0"
+VERSION = "1.2.0a"
 if "a" in VERSION:
     VERSION += "+" + subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
 
