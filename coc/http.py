4d8bab52f01b3a336855359436f8c8f40ff33a26
mathsman5133 <38833292+mathsman5133@users.noreply.github.com>
2022-06-22 17:03:43 +1000

    Update http.py

--- coc/http.py
+++ coc/http.py
@@ -425,8 +425,7 @@ class HTTPClient:
         LOG.info("Successfully logged into the developer site.")
 
         resp_paylaod = await resp.json()
-        ip = json_loads(base64_b64decode(resp_paylaod["temporaryAPIToken"].split(".")[1] + "====").decode("utf-8"))[
-            "limits"][1]["cidrs"][0].split("/")[0]
+        ip = json_loads(base64_b64decode(resp_paylaod["temporaryAPIToken"].split(".")[1] + "====").decode("utf-8"))["limits"][1]["cidrs"][0].split("/")[0]
 
         LOG.info("Found IP address to be %s", ip)
 
