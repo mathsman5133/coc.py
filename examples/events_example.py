e5d9fd8423144510358a7f690cc82d8e9f83bbb4
doobie <sgtmajordoobie@live.com>
2022-08-25 13:53:27 -0400

    [Updated events_example.py] to use a loop runner and use the registration methods that coc.EventsClient has isntead of the lists

--- examples/events_example.py
+++ examples/events_example.py
@@ -2,25 +2,26 @@ import asyncio
 import logging
 import os
 
-from dotenv import load_dotenv
-
 import coc
 from coc import utils
 
 clan_tags = ["#P222C9Y", "#9VPR98RG", "#9G2QU8YG", "#80Y8L0QY", "#2Y28CGP8"]
 
-
 """Clan Events"""
+
+
 @coc.ClanEvents.member_donations()
 async def on_clan_member_donation(old_member, new_member):
     final_donated_troops = new_member.donations - old_member.donations
-    log.info(f"{new_member} of {new_member.clan} just donated {final_donated_troops} troops.")
+    log.info(
+        f"{new_member} of {new_member.clan} just donated {final_donated_troops} troops.")
 
 
 @coc.ClanEvents.member_received()
 async def on_clan_member_donation_receive(old_member, new_member):
     final_received_troops = new_member.received - old_member.received
-    log.info(f"{new_member} of {new_member.clan} just received {final_received_troops} troops.")
+    log.info(
+        f"{new_member} of {new_member.clan} just received {final_received_troops} troops.")
 
 
 @coc.ClanEvents.member_join()
@@ -35,45 +36,58 @@ async def on_clan_member_leave(member, clan):
 
 @coc.ClanEvents.points()
 async def on_clan_trophy_change(old_clan, new_clan):
-    log.info(f"{new_clan.name} total trophies changed from {old_clan.points} to {new_clan.points}")
+    log.info(
+        f"{new_clan.name} total trophies changed from {old_clan.points} to {new_clan.points}")
 
 
 @coc.ClanEvents.member_versus_trophies()
 async def clan_member_versus_trophies_changed(old_member, new_member):
-    log.info(f"{new_member} versus trophies changed from {old_member.versus_trophies} to {new_member.versus_trophies}")
+    log.info(
+        f"{new_member} versus trophies changed from {old_member.versus_trophies} to {new_member.versus_trophies}")
 
 
 """War Events"""
+
+
 @coc.WarEvents.war_attack()
 async def current_war_stats(attack, war):
-    log.info(f"Attack number {attack.order}\n({attack.attacker.map_position}).{attack.attacker} of {attack.attacker.clan} "
-             f"attacked ({attack.defender.map_position}).{attack.defender} of {attack.defender.clan}")
+    log.info(
+        f"Attack number {attack.order}\n({attack.attacker.map_position}).{attack.attacker} of {attack.attacker.clan} "
+        f"attacked ({attack.defender.map_position}).{attack.defender} of {attack.defender.clan}")
 
 
 """Player Events"""
+
+
 @coc.PlayerEvents.donations()
 async def on_player_donation(old_player, new_player):
     final_donated_troops = new_player.donations - old_player.donations
-    log.info(f"{new_player} of {new_player.clan} just donated {final_donated_troops} troops.")
+    log.info(
+        f"{new_player} of {new_player.clan} just donated {final_donated_troops} troops.")
 
 
 @coc.PlayerEvents.received()
 async def on_player_donation_receive(old_player, new_player):
     final_received_troops = new_player.received - old_player.received
-    log.info(f"{new_player} of {new_player.clan} just received {final_received_troops} troops.")
+    log.info(
+        f"{new_player} of {new_player.clan} just received {final_received_troops} troops.")
 
 
 @coc.PlayerEvents.trophies()
 async def on_player_trophy_change(old_player, new_player):
-    log.info(f"{new_player} trophies changed from {old_player.trophies} to {new_player.trophies}")
+    log.info(
+        f"{new_player} trophies changed from {old_player.trophies} to {new_player.trophies}")
 
 
 @coc.PlayerEvents.versus_trophies()
 async def on_player_versus_trophy_change(old_player, new_player):
-    log.info(f"{new_player} versus trophies changed from {old_player.trophies} to {new_player.trophies}")
+    log.info(
+        f"{new_player} versus trophies changed from {old_player.trophies} to {new_player.trophies}")
 
 
 """Client Events"""
+
+
 @coc.ClientEvents.maintenance_start()
 async def on_maintenance():
     log.info("Maintenace Started")
@@ -86,20 +100,33 @@ async def on_maintenance_completion(time_started):
 
 @coc.ClientEvents.new_season_start()
 async def season_started():
-    log.info("New season started, and will finish at %s", str(utils.get_season_end()))
+    log.info("New season started, and will finish at %s",
+             str(utils.get_season_end()))
 
 
-async def main(client: coc.EventsClient) -> None:
+async def main() -> None:
+    # Instantiate the coc_client using the Events client
+    coc_client = coc.EventsClient(
+        key_names="coc.py bot example"
+    )
+
+    # Attempt to log into CoC API using your credentials.
+    try:
+        await coc_client.login(os.environ.get("DEV_SITE_EMAIL"),
+                               os.environ.get("DEV_SITE_PASSWORD"))
+    except coc.InvalidCredentials:
+        exit("[!] Invalid credentials used")
+
     # Register all the clans you want to track
-    client.add_clan_updates(*clan_tags)
+    coc_client.add_clan_updates(*clan_tags)
 
     # Register all the players you want to track
-    async for clan in client.get_clans(clan_tags):
-        client.add_player_updates(*[member.tag for member in clan.members])
+    async for clan in coc_client.get_clans(clan_tags):
+        coc_client.add_player_updates(*[member.tag for member in clan.members])
 
     # Register all the callback functions that are triggered when a
     # event if fired.
-    client.add_events(
+    coc_client.add_events(
         on_clan_member_donation,
         on_clan_member_donation_receive,
         on_clan_member_join,
@@ -116,55 +143,30 @@ async def main(client: coc.EventsClient) -> None:
         season_started
     )
 
+    if os.environ.get("RUNNING_TESTS"):
+        # ignore this; it's just for running github action tests.
+        import sys
+
+        class Handler(logging.Handler):
+            def emit(self, record) -> None:
+                sys.exit(0)
+
+        log.addHandler(Handler())
+        # we don't wanna wait forever for an event, so if
+        # it sets up OK lets call it quits.
+        coc_client.loop.call_later(20.0, sys.exit, 0)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger()
 
-    # if os.environ.get("RUNNING_TESTS"):
-    #     # ignore this; it's just for running github action tests.
-    #     import sys
-    #     client = coc.login(
-    #         os.environ["DEV_SITE_EMAIL"],
-    #         os.environ["DEV_SITE_PASSWORD"],
-    #         key_names="coc.py tests",
-    #         client=coc.EventsClient,
-    #     )
-    #
-    #     class Handler(logging.Handler):
-    #         def emit(self, record) -> None:
-    #             sys.exit(0)
-    #     log.addHandler(Handler())
-    #     # we don't wanna wait forever for an event, so if it sets up OK lets call it quits.
-    #     client.loop.call_later(20.0, sys.exit, 0)
-
-    load_dotenv()
-
-    # Instantiate the coc_client using the Events client
-    coc_client = coc.EventsClient(
-        key_names="coc.py bot example"
-    )
-
-    # Manually create loop object instead of asyncio.run() for Events
     loop = asyncio.get_event_loop()
 
-    # Establish a connection with the coc API
-    loop.run_until_complete(
-        coc_client.login(os.environ.get("DEV_SITE_EMAIL"),
-                         os.environ.get("DEV_SITE_PASSWORD")))
-
-    loop.run_until_complete(main(coc_client))
-
     try:
+        # Using the loop context, run the main function then set the loop
+        # to run forever so that it continuously monitors for events
+        loop.run_until_complete(main())
         loop.run_forever()
     except KeyboardInterrupt:
-        pass  # Ignore keyboard interrupt
-    finally:
-        # Loop over all tasks and cancel them properly to clean up
-        loop.run_until_complete(coc_client.close())
-        for task in asyncio.all_tasks(loop):
-            task.cancel()
-        # Take the time to clean up generators
-        loop.run_until_complete(loop.shutdown_asyncgens())
-        # Close loop
-        loop.close()
+        pass
