5f62057cdc87c38fc5ec46e8faaa2d62b11d8c0a
doobie <sgtmajordoobie@live.com>
2022-08-25 14:07:52 -0400

    [Updated war_logs.py] Updated war_log example to use asyncio.run() wrapper

--- examples/war_logs.py
+++ examples/war_logs.py
@@ -3,14 +3,12 @@ import os
 
 import coc
 
-# email and password is your login credentials used at https://developer.clashofclans.com
-client = coc.login(os.environ["DEV_SITE_EMAIL"], os.environ["DEV_SITE_PASSWORD"], key_names="coc.py tests")
 
-
-async def get_warlog_for_clans(clan_tags: list):
+async def get_warlog_for_clans(client: coc.Client, clan_tags: list):
     war_logs = {}
     for tag in clan_tags:
-        # if we're not allowed to view warlog (private in game), this will raise an exception
+        # if we're not allowed to view warlog (private in game),
+        # this will raise an exception
         try:
             warlog = await client.get_warlog(tag)
         except coc.PrivateWarLog:
@@ -18,21 +16,21 @@ async def get_warlog_for_clans(clan_tags: list):
 
         war_logs[tag] = warlog
 
-    # return a dict of list of war logs: {'tag': [list_of_warlog_objects]}
     return war_logs
 
 
-async def get_clan_tags_names(name: str, limit: int):
+async def get_clan_tags_names(client: coc.Client, name: str, limit: int):
     clans = await client.search_clans(name=name, limit=limit)
-    # return a list of tuples of name/tag pair ie. [(name, tag), (another name, another tag)]
+    # return a list of tuples of name/tag pair ie.
+    # [(name, tag), (another name, another tag)]
     return [(n.name, n.tag) for n in clans]
 
 
-async def get_warlog_opponents_from_clan_name(name: str, no_of_clans: int):
-    clan_tags_names = await get_clan_tags_names(name, no_of_clans)
+async def get_warlog_opponents_from_clan_name(client: coc.Client, name: str, no_of_clans: int):
+    clan_tags_names = await get_clan_tags_names(client, name, no_of_clans)
 
     # search for war logs with clan tags found
-    war_logs = await get_warlog_for_clans([n[1] for n in clan_tags_names])
+    war_logs = await get_warlog_for_clans(client, [n[1] for n in clan_tags_names])
 
     for name, tag in clan_tags_names:
         # iterate over the wars
@@ -45,6 +43,18 @@ async def get_warlog_opponents_from_clan_name(name: str, no_of_clans: int):
                 print("War: {} vs {}".format(war.clan.name, war.opponent.name))
 
 
+async def main():
+    coc_client = coc.Client()
+
+    try:
+        await coc_client.login(os.environ.get("DEV_SITE_EMAIL"),
+                               os.environ.get("DEV_SITE_PASSWORD"))
+    except coc.InvalidCredentials:
+        exit("[!] Invalid credentials used")
+
+    await get_warlog_opponents_from_clan_name(coc_client, "Reddit Zulu", 5)
+    await coc_client.close()
+
+
 if __name__ == "__main__":
-    client.loop.run_until_complete(get_warlog_opponents_from_clan_name("name", 5))
-    client.close_loops()
+    asyncio.run(main())
