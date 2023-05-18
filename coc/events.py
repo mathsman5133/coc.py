01360894bbc55eb33ad7119598bf544a482d881f
tammes <tabu.der.erste@gmx.de>
2023-05-18 14:21:31 +0200

    bump version numbers and add changelog

--- coc/events.py
+++ coc/events.py
@@ -83,7 +83,7 @@ class _ValidateEvent:
             return self.cls.__getattr__(self.cls, item)
 
         # handle member_x events:
-        if "member_" in item and item != "member_count":
+        if "member_" in item:
             item = item.replace("member_", "")
             nested = True
         else:
