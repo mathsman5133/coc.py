aeeb730a182bb12d89686e7707683ab553779f9a
doluk <69309597+doluk@users.noreply.github.com>
2024-04-27 06:46:44 +0200

    Update version

--- coc/__init__.py
+++ coc/__init__.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__version__ = "3.4.2"
+__version__ = "3.5.1"
 
 from .abc import BasePlayer, BaseClan
 from .clans import RankedClan, Clan
